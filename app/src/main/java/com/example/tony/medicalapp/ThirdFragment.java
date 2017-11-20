4b7f2e52f50408518cc7656166c11df1b91ef113
ragan2 <ragan1@student.gsu.edu>
2017-11-20 17:17:07 -0500

    hi

--- app/src/main/java/com/example/tony/medicalapp/ThirdFragment.java
+++ app/src/main/java/com/example/tony/medicalapp/ThirdFragment.java
@@ -1,6 +1,7 @@
 package com.example.tony.medicalapp;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -19,5 +20,10 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.third_layout, container, false);
         return myView;
+
+    }
+    public void addNew (View view){
+        Intent intent = new Intent (this, second_layout.xml);
+        startActivity(intent);
     }
 }
